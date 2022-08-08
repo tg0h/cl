323c69933972962bf28022a96d4d850a8de0c0a9
tim goh <timslgoh@gmail.com>
2022-08-08 16:16:31 +0800

    feat: add filter pattern arg

--- helper/cwClient.js
+++ helper/cwClient.js
@@ -1,12 +1,11 @@
-import {convertDate,formatDate} from './time.js'
-import {DateTime} from 'luxon'
-
+import { convertDate, formatDate } from "./time.js";
+import { DateTime } from "luxon";
 
 import {
-    CloudWatchLogsClient,
-    GetLogEventsCommand,
-    FilterLogEventsCommand,
-    paginateFilterLogEvents
+  CloudWatchLogsClient,
+  GetLogEventsCommand,
+  FilterLogEventsCommand,
+  paginateFilterLogEvents,
 } from "@aws-sdk/client-cloudwatch-logs"; // ES Modules import
 
 // const client = new CloudWatchLogsClient({region: "ap-southeast-1"});
@@ -14,37 +13,41 @@ import {
 const client = new CloudWatchLogsClient();
 
 const paginatorConfig = {
-    client,
-    // pageSize: 25
-}
+  client,
+  // pageSize: 25
+};
 
-const results = []
+const results = [];
 
 let run = async function (argv) {
+  // 1 hour ago
+  let defaultStart = Date.now() - 10 * 60 * 1000;
 
-    // 1 hour ago
-    let defaultStart = Date.now() - 10 * 60 * 1000
+  let start = argv.start ? convertDate(argv.start) : defaultStart;
+  let end = argv.end ? convertDate(argv.end) : Date.now();
+  let filterPattern = argv.pattern;
+  console.log("start", formatDate(start));
+  console.log("end", formatDate(end));
+  let input = {
+    logGroupName: argv.logName,
+    startTime: start,
+    endTime: end,
+  };
 
-    let start = argv.start ? convertDate(argv.start) : defaultStart
-    let end = argv.end ? convertDate(argv.end) : Date.now()
-    console.log('start', formatDate(start))
-    console.log('end', formatDate(end))
-    let input = {
-        logGroupName: argv.logName,
-        startTime: start,
-        endTime: end
-    }
+  if (filterPattern) {
+    input.filterPattern = filterPattern;
+  }
 
-    const paginator = paginateFilterLogEvents(paginatorConfig, input);
+  const paginator = paginateFilterLogEvents(paginatorConfig, input);
 
-    let i = 1
-    for await (const page of paginator) {
-        i++
-        // page contains a single paginated output.
-        results.push(...page.events);
-        // console.log('this is a ',page)
-    }
-    return results
-}
+  let i = 1;
+  for await (const page of paginator) {
+    i++;
+    // page contains a single paginated output.
+    results.push(...page.events);
+    // console.log('this is a ',page)
+  }
+  return results;
+};
 
-export {run}
+export { run };

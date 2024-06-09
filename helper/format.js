daa3770deb7b372feaa85c4d46f18f48badf9dcb
tim goh <timslgoh@gmail.com>
2024-06-09 09:59:53 +0800

    feat: -p adds sg time header

--- helper/format.js
+++ helper/format.js
@@ -1,19 +1,22 @@
-import {formatDate} from './time.js'
-import chalk from 'chalk';
+import { formatDate } from "./time.js";
+import chalk from "chalk";
 
-function format(result) {
+function format(result, { prefixSgTime }) {
+  // row.eventId
+  // row.ingestionTime
+  // row.logStreamName
+  // row.timestamp
 
-    // row.eventId
-    // row.ingestionTime
-    // row.logStreamName
-    // row.timestamp
-
-    result.forEach((row) => {
-        // console.log(formatDate(row.timestamp), row.message);
-        let t = formatDate(row.timestamp)
-        // console.log('t',chalk.green(t))
-        console.log(chalk.green(t), row.message);
-    })
+  result.forEach((row) => {
+    // console.log(formatDate(row.timestamp), row.message);
+    let t = formatDate(row.timestamp);
+    // console.log('t',chalk.green(t))
+    if (prefixSgTime) {
+      console.log(chalk.green(t), row.message);
+    } else {
+      console.log(row.message);
+    }
+  });
 }
 
-export {format}
+export { format };
--- index.js
+++ index.js
@@ -1,41 +1,52 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
-import {hideBin} from "yargs/helpers";
-import {run} from "./helper/cwClient.js"
-import {convertDate} from "./helper/time.js"
-import {format} from "./helper/format.js"
+import { hideBin } from "yargs/helpers";
+import { run } from "./helper/cwClient.js";
+import { convertDate } from "./helper/time.js";
+import { format } from "./helper/format.js";
 
 let argv = yargs(hideBin(process.argv))
-    .usage("$0 <logName> [pattern] [options]")
-    // arrow brackets indicate that logname is mandatory, running cl without
-    // the logName then runs the help automatically
-    .command('$0 <logName> [pattern]', 'search cloudwatch logs', (yargs) => {
-            yargs.positional('logName', {
-                describe: 'cloudwatch log name to get aoeu aoenuho\n aoeuaoeu aoeuthhusnthaoeu \n',
-                type: 'string',
-            }).positional('pattern', {
-                describe: 'regex pattern to filter logs by'
-            })
-        }, async (argv) =>{
-        // console.log('is this argv',argv)
-        // console.log('argv log name',argv.logName)
-            let result = await run(argv)
-             format(result)
-        }
-    )
-    .option("s", {
-        alias: "start",
-        describe: "start parameter",
-    })
-    .option("e", {
-        alias: "end",
-    })
-    .option("r", {
-        alias: "range",
-    })
-    .argv
+  .usage("$0 <logName> [pattern] [options]")
+  // arrow brackets indicate that logname is mandatory, running cl without
+  // the logName then runs the help automatically
+  .command(
+    "$0 <logName> [pattern]",
+    "search cloudwatch logs",
+    (yargs) => {
+      yargs
+        .positional("logName", {
+          describe:
+            "cloudwatch log name to get aoeu aoenuho\n aoeuaoeu aoeuthhusnthaoeu \n",
+          type: "string",
+        })
+        .positional("pattern", {
+          describe: "regex pattern to filter logs by",
+        });
+    },
+    async (argv) => {
+      // console.log('is this argv',argv)
+      // console.log('argv log name',argv.logName)
+      let result = await run(argv);
 
-
+      let options = { prefixSgTime: argv.prefixSgTime };
+      format(result, options);
+    },
+  )
+  .option("s", {
+    alias: "start",
+    describe: "start parameter",
+  })
+  .option("e", {
+    alias: "end",
+  })
+  .option("r", {
+    alias: "range",
+  })
+  .option("prefixSgTime", {
+    alias: "p",
+    type: "boolean",
+    default: false,
+  }).argv;
 
 // run()
